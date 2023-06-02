42a0988ae92fc900dbca2982030881a890e32ed0
Augusto Borges <99897654+augustbrgs@users.noreply.github.com>
2023-06-02 14:15:08 -0300

    section 4

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,23 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import React, { useState} from "react";
+function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  function clickHandler(){
+    setTitle('Updated!');
+  }
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
